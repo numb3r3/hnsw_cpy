45f4a99ec5325ded38404ba2da0697942ab13f6e
hanhxiao <hanhxiao@tencent.com>
2019-01-25 10:50:35 +0800

    refactor

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from bt_index.indexer import BTIndex
+from bt_index import BTIndex
+from bt_index.helper import get_run_args
 
 if __name__ == '__main__':
-    with open('test.bin', 'rb') as fp:
-        bt = BTIndex(fp.read(), bytes_per_vector=96)
+    args = get_run_args()
+    bt = BTIndex(args.binary_file.read(), args.bytes_per_vector)
     print(vars(bt.stat))
     print(bt.get_all_list_len())
