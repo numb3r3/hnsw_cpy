e31e82cef0a5805be06c4ec4a01b4b840254ac2b
hanhxiao <hanhxiao@tencent.com>
2019-01-31 18:40:29 +0800

    update cli

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     args = get_run_args()
     bt = BIndex(args.bytes_per_vector)
     bt.add(args.binary_file.read(), num_rows=args.num_vector)
-    print(vars(bt.statistic))
+    print(bt.statistic)
