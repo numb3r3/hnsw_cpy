54380faa69f14a1623892546ef611ea76b9716c3
hanhxiao <hanhxiao@tencent.com>
2019-01-31 19:10:25 +0800

    fix realloc

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
         bt = BIndex(args.bytes_per_vector)
         start_t = time.perf_counter()
         bt.add(args.binary_file.read(), num_rows=num_vector)
-        time_cost = time.perf_counter() - start_t
-        mem_cost = bt.memory_size
+        t_avg = time.perf_counter() - start_t
+        m_avg = bt.memory_size
         unique_rate = int(100 * bt.statistic['num_unique_keys'] / bt.statistic['num_keys'])
-        qps = int(num_vector / time_cost)
-        print(f'{num_vector}\t{qps}\t{time_cost}\t{m_avg}\t{bt.statistic["num_unique_keys"]}\t{unique_rate}')
+        qps = int(num_vector / t_avg)
+        print(f'{num_vector}\t{qps}\t{t_avg}\t{m_avg}\t{bt.statistic["num_unique_keys"]}\t{unique_rate}')
         num_vector *= 2
