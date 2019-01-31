052fdf050787707b2869b5b37375c65952dbff2b
hanhxiao <hanhxiao@tencent.com>
2019-01-31 19:35:48 +0800

    fix realloc

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from bindex import BIndex
-from bindex.helper import get_run_args
+from bindex.helper import get_run_args, TimeContext
 
 if __name__ == '__main__':
     args = get_run_args()
     bt = BIndex(args.bytes_per_vector)
-    bt.add(args.binary_file.read(), args.num_data)
-    r = bt.find(args.binary_file.read(), args.num_query)
-    print(r)
+    with TimeContext('building index...'):
+        bt.add(args.data_file.read(), args.num_data)
+
+    with TimeContext('searching query...'):
+        r = bt.find(args.query_file.read(), args.num_query)
+        print(r)
