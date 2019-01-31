b92c10c7949f5a2b1164bc31036becf6d3f44433
hanhxiao <hanhxiao@tencent.com>
2019-01-31 19:11:55 +0800

    fix realloc

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from bindex.helper import get_run_args
 
 if __name__ == '__main__':
     args = get_run_args()
-
     num_vector = 8192
     print('data size\tQPS\ttime(s)\tmemory\tunique keys\tunique rate')
     bt = BIndex(args.bytes_per_vector)
+    bin_data = args.binary_file.read()
     for _ in range(10):
         start_t = time.perf_counter()
-        bt.add(args.binary_file.read(), num_rows=num_vector)
+        bt.add(bin_data, num_rows=num_vector)
         t_avg = time.perf_counter() - start_t
         m_avg = bt.memory_size
         unique_rate = int(100 * bt.statistic['num_unique_keys'] / bt.statistic['num_keys'])
